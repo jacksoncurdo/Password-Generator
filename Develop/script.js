fbbaf4ede5d44003bb744b90f8f15ce2a2ff0fd9
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-01-15 15:26:00 -0700

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,22 @@
-// Assignment Code
+var numbers = "0123456789";
+var specialChars = "!@#$%^&*()_+~`|}{[];:?/-=";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = lowercase.toUpperCase
+var upperCaseResponse = false;
+var numbersResponse = false;
+var specialCharsResponse = false;
+var lowerCaseResponse = false;
+var desiredLength = 0;
+var includedCharContainer = ""
+var newPassword = ""
+
+
+
+
+
+
+
+// 
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
