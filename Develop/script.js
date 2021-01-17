026af6dc19fe36e61b9e1472f9b5c21cb84b6768
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-01-17 16:41:44 -0700

    changed desiredlength in the function options

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 var numbers = "0123456789";
 var specialChars = "!@#$%^&*()_+~`|}{[];:?/-=";
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = lowercase.toUpperCase
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = lowerCase.toUpperCase();
 var upperCaseResponse = false;
 var numbersResponse = false;
 var specialCharsResponse = false;
@@ -10,28 +10,35 @@ var desiredLength = 0;
 var includedCharContainer = ""
 var newPassword = ""
 
-function options () {
-  desiredLength = prompt("Please choose the length of your password ( must be between 8 and 12 characters ) ")
+function options() {
+  desiredLength = prompt("Please choose the length of your password ( must be between 8 and 128 characters).")
+  if (desiredLength > 7 && desiredLength < 129) {
+    alert('your password will contain ${desiredlength} characters')
+  } else {
+    alert("your character count is invalid")
+    desiredLength = prompt("Please choose the length of your password (must be between 8 and 128 characters). ")
+  }
+
   upperCaseResponse = confirm("do you want to include upper case letters?")
   numbersResponse = confirm("do you want to include numbers?")
   specialCharsResponse = confirm("do you want to include special characters?")
   lowerCaseResponse = confirm("do you want to include lower case letters?")
 }
 
-function createCharacterOptions(){
+function createCharacterOptions() {
   if (upperCaseResponse == true)
-  includedCharContainer = includedCharContainer.concat(upperCase)
+    includedCharContainer = includedCharContainer.concat(upperCase)
   if (numbersResponse == true)
-  includedCharContainer = includedCharContainer.concat(numbers)
+    includedCharContainer = includedCharContainer.concat(numbers)
   if (specialCharsResponse == true)
-  includedCharContainer = includedCharContainer.concat(specialChars)
+    includedCharContainer = includedCharContainer.concat(specialChars)
   if (lowerCaseResponse == true)
-  includedCharContainer = includedCharContainer.concat(lowerCase)
+    includedCharContainer = includedCharContainer.concat(lowerCase)
 }
 
-function generatePassword(){
-  for (i=0; i < desiredLength; i++){
-    var random = Math.floor(Math.random()*includedCharContainer.length);
+function generatePassword() {
+  for (i = 0; i < desiredLength; i++) {
+    var random = Math.floor(Math.random() * includedCharContainer.length);
     newPassword = newPassword + includedCharContainer[random];
   }
   return newPassword
