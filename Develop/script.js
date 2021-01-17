47f884c43f3dbe43078b8fd74d6c2ceeae2a3ec7
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-01-17 09:56:46 -0700

    added function options

--- Develop/script.js
+++ Develop/script.js
@@ -10,8 +10,13 @@ var desiredLength = 0;
 var includedCharContainer = ""
 var newPassword = ""
 
-
-
+function options () {
+  desiredLength = prompt("")
+  upperCaseResponse = confirm("do you want to include upper case letters?")
+  numbersResponse = confirm("do you want to include numbers?")
+  specialCharsResponse = confirm("do you want to include special characters?")
+  lowerCaseResponse = confirm("do you want to include lower case letters?")
+}
 
 
 
